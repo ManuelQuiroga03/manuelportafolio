1a08c8ae91b950eca2f851ec81abb240be3371ca
Manuel Quiroga <quirogamanuel21@gmail.com>
2023-11-23 20:09:22 -0600

    cdns

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,53 @@
+<!-- Portafolio.svelte -->
 <script>
+	import Navbar from "./Navbar/navbar.svelte";
+
+
 	export let name;
+	name = 'Manuel Hernández Quiroga';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Navbar></Navbar>
+	<!-- <h1>Hola, soy {name}.</h1>
+	<p>Soy Ingeniero en Informática especializado en el desarrollo de software.</p>
+	<p>Tengo habilidades en desarrollo Frontend y Backend, incluyendo:</p>
+	<ul>
+		<li>HTML, CSS, JavaScript (y frameworks como React o Vue para Frontend)</li>
+		<li>Node.js, Express (para Backend)</li>
+		<li>Bases de datos SQL (como MySQL, PostgreSQL)</li>
+	</ul>
+	<p>Puedes ver más sobre mi trabajo en mi <a href="https://github.com/tuusuario">perfil de GitHub</a>.</p> -->
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 600px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		color: #3366ff;
+		font-size: 3em;
+		font-weight: 600;
+		margin-bottom: 0.5em;
 	}
 
-	@media (min-width: 640px) {
+	p {
+		font-size: 1.2em;
+		line-height: 1.6;
+	}
+
+	ul {
+		text-align: left;
+		margin-left: 2em;
+	}
+
+	@media (min-width: 768px) {
 		main {
-			max-width: none;
+			max-width: 800px;
 		}
 	}
 </style>
---
+++ src/Navbar/navbar.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<main>
+    <div class="container">
+        <nav>
+            <div class="logo">
+
+            </div>
+            <div class="nav_links">
+                Hola
+            </div>
+            <div class="social-icons">
+                <!-- <a href="URL_a_tu_LinkedIn" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href="URL_a_tu_Twitter" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+                <a href="URL_a_tu_GitHub" target="_blank"><FontAwesomeIcon icon={faGithub} /></a> -->
+            </div>
+        </nav>
+    </div>
+</main>
+
+<style>
+
+</style>
