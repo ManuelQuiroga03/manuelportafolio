5e772c2fbe3c739b429beb9744446749a63de1b9
Manuel Quiroga <quirogamanuel21@gmail.com>
2023-11-23 22:52:13 -0600

    Header,Footer,contenido

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,129 @@
 <!-- Portafolio.svelte -->
 <script>
 	import Navbar from "./Navbar/navbar.svelte";
-
+	import Footer from "./Footer/footer.svelte";
 
 	export let name;
 	name = 'Manuel Hernández Quiroga';
 </script>
 
-<main>
-	<Navbar></Navbar>
+<div class="contenedor">
+	<div class="">
+		<Navbar></Navbar>
+	</div>
 	
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<span>
-		<i class="fa-regular fa-user"></i>
-	</span>
-</main>
+	<div class="contentMe">
+		<div class="heroMe">
+			<div>
+				<img class="imgName_CustomImage" src="images/imgPerfil.JPG" alt="Manuel Quiroga">
+			</div>
+		</div>
+		<div class="nameMe">
+			<h1>
+				<span style="color: #243746;">Manuel</span> <span style="color: gray;">Quiroga</span>
+			</h1>
+		</div>
+		<div class="infoMe mt-4 mb-4">
+			<p>Desarrollador Jr en Ximplify</p>
+			<p>"El software es un gran combinador; te permite combinar tus ideas con la capacidad de la máquina y ver los resultados de inmediato". - Hal Abelson</p>
+			<p></p>
+		</div>
+		<div class="">
+			<div class="text-center mt-4 mb-4">
+				<span>Mis Proyectos</span>
+			</div>
+			<div class="grid-container mt-4 mb-4 ps-4 pe-4">
+				<!-- Tarjeta 1 -->
+				<div class="card">
+					<img src="ruta/a/imagen-proyecto-1.jpg" alt="Proyecto 1">
+					<h3>Título del Proyecto 1</h3>
+					<p>Descripción breve del Proyecto 1.</p>
+				</div>
+				<div class="card">
+					<img src="ruta/a/imagen-proyecto-1.jpg" alt="Proyecto 1">
+					<h3>Título del Proyecto 1</h3>
+					<p>Descripción breve del Proyecto 1.</p>
+				</div>
+				<div class="card">
+					<img src="ruta/a/imagen-proyecto-1.jpg" alt="Proyecto 1">
+					<h3>Título del Proyecto 1</h3>
+					<p>Descripción breve del Proyecto 1.</p>
+				</div>
+				
+			
+				<!-- Tarjeta 2 -->
+				<div class="card">
+					<img src="ruta/a/imagen-proyecto-2.jpg" alt="Proyecto 2">
+					<h3>Título del Proyecto 2</h3>
+					<p>Descripción breve del Proyecto 2.</p>
+				</div>
+			
+				<!-- ... Repite este bloque para cada tarjeta -->
+			</div>
+		</div>
+	</div>
+
+	<div class="">
+		<Footer></Footer>
+	</div>
+</div>
 
 <style>
-	main {
+	.contenedor{
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		
+	}
+	.container{
+		margin: 0;
+		padding: 0;
+	}
+	.heroMe{
 		text-align: center;
-		padding: 1em;
-		max-width: 600px;
-		margin: 0 auto;
+		margin-top: 20px;
 	}
-
-	h1 {
-		color: #3366ff;
-		font-size: 3em;
+	.imgName_CustomImage{
+        border-radius: 50%;
+        width: 150px;
+        height: 150px;
+        object-fit: contain;
+    }
+	.nameMe{
+		text-align: center;
+		margin-top: 30px;
+	}
+	h1{
+		font-size: 50px;
+		font-weight: 700;
+	}
+	.infoMe{
+		text-align: center;
+		font-size: 25px;
 		font-weight: 600;
-		margin-bottom: 0.5em;
+		margin-left: 15px;
+		margin-right: 15px;
 	}
-
-	p {
-		font-size: 1.2em;
-		line-height: 1.6;
+	.contentMe{
+		padding-left: 0.5rem;
+		padding-right: 0.5rem;
 	}
-
-	ul {
-		text-align: left;
-		margin-left: 2em;
-	}
-
-	@media (min-width: 768px) {
-		main {
-			max-width: 800px;
-		}
-	}
+	.grid-container {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+		grid-auto-rows: minmax(200px, auto);
+        gap: 20px;
+        /* Otros estilos del grid */
+    }
+    .card {
+        /* Estilos de la tarjeta */
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 20px;
+    }
+    .card img {
+        width: 100%; /* Ajusta el tamaño de la imagen */
+        border-radius: 8px; /* Bordes redondeados para la imagen */
+        margin-bottom: 15px; /* Espaciado entre la imagen y el texto */
+    }
 </style>
--- src/Navbar/navbar.svelte
+++ src/Navbar/navbar.svelte
@@ -1,24 +1,87 @@
 <script>
+
 </script>
 
-<main>
-    <div class="container">
-        <nav>
-            <div class="logo">
 
+    <div class="mx-auto">
+        <header class="header">
+            <div class="imgName">
+                <div>
+                    <img class="imgName_CustomImage" src="images/imgPerfil.JPG" alt="Manuel Quiroga">
+                </div>
+                <div>
+                    <span>Manuel Quiroga</span>
+                </div>
             </div>
             <div class="nav_links">
-                Hola
+                <ul>
+                    <li>
+                        <a href="">About</a>
+                        <a href="">Courses</a>
+                        <a href="">Projects</a>
+                        <a href="">Contact</a>
+                    </li>
+                </ul>
             </div>
             <div class="social-icons">
-                <!-- <a href="URL_a_tu_LinkedIn" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href="URL_a_tu_Twitter" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
-                <a href="URL_a_tu_GitHub" target="_blank"><FontAwesomeIcon icon={faGithub} /></a> -->
+                <div class="custom_Icon"><i class="fa-brands fa-linkedin" style="color: white;"></i></div>
+                <div class="custom_Icon"><i class="fa-brands fa-square-github" style="color: white;"></i></div>
+                <div class="custom_Icon"><i class="fa-regular fa-sun" style="color: white;"></i></div>
+                <div class="custom_Icon"><i class="fa-solid fa-moon" style="color: white;"></i></div>
             </div>
-        </nav>
+        </header>
     </div>
-</main>
 
 <style>
-
+    ul{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    li{
+        text-decoration: none;
+    }
+    a{
+        text-decoration: none;
+        color: white;
+        margin-right: 15px;
+        font-size: 20px;
+        font-weight: 600;
+    }
+    .mx-auto{
+        margin-left: auto;
+        margin-right: auto;
+        align-items: center;
+        background-color: #091A28;
+    }
+    .header{
+        display: flex;
+        justify-content: space-between;
+        margin-left: 10px;
+        margin-right: 20px;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        align-items: center;
+    }
+    /* Sección Imagen y nombre */
+    .imgName{
+        display: flex;
+        gap: 16px;
+        align-items: center;
+        color: white;
+    }
+    .imgName_CustomImage{
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        object-fit: cover;
+    }
+    .social-icons{
+        display: flex;
+        justify-content: end;
+        gap: 15px;
+    }
+    .custom_Icon{
+        font-size: 1.5rem;
+    }
 </style>
